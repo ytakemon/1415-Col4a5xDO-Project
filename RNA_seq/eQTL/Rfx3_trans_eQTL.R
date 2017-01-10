b5dea1cd20746cb33b56f730daf6a0b83a8ef674
ytakemon <yuka.takemon@gmail.com>
2017-01-10 14:30:11 -0500

    Script for trans eQTL for Rfx3

--- RNA_seq/eQTL/Rfx3_trans_eQTL.R
+++ RNA_seq/eQTL/Rfx3_trans_eQTL.R
@@ -1,11 +1,10 @@
 ################################################################################################################################
-## Identifying trans-eQTL for Formin 1 (Fmn1)
+## Identifying trans-eQTL for Rfx3
 ## Yuka Takemon
-## Created: 12/14/16
-## Last modified: 12/20/16
+## Created: 01/10/17
 
-## This script will identify trans-eQTL for Formin 1 by using the eQTL scanone objects created by eQTL_all.R Each scanone eQTL 
-##	object will be assessed for significant peaks at Fmn1, more specifically at  "JAX00098823" or "rs27466023". Sigificance will
+## This script will identify trans-eQTL for Rfx3 by using the eQTL scanone objects created by eQTL_all.R Each scanone eQTL 
+##	object will be assessed for significant peaks at Rfx3, more specifically at  "JAX00098823" or "rs27466023". Sigificance will
 ## 	be determined by their LOD scores above the pre-calulated LOD scores:
 ## > threshold
 ## sig.level         A        X
@@ -13,18 +12,16 @@
 ## 0.1  		7.632182 9.048455
 ## 0.63 		6.047843 7.149671
 ## since we have 3 levels of significance we will be make 3 lists one for each threshold to better narrow down candidates that 
-## have a trans-eQTL at Fmn1 ( "JAX00098823" or "rs27466023")
+## have a trans-eQTL at Rfx3
 
-## Note:
-## This script is tailored specifically for FMN1 however by changing marker parameters, we can apply this to find trans-eQTL for
-## any genes. If I find time later, I can alter the script to make it more adaptable.
+#module load R/3.1.1
 
 #	load library
 library(DOQTL)
 setwd("/hpcdata/ytakemon/Col4a5xDO")
 
 #	Helperfuntions used:
-source("substrR_L.R")
+#source("substrR_L.R")
 
 #	Input data
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/genoprobs/best.genoprobs.192.Rdata")
@@ -32,6 +29,8 @@ load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/RNA_seq_Rdata/RNA
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/EnsemblID_GRCm38.p4.Rdata")
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/perm1000/perms.1000.qtl.RNA.rankZ.tpm.Rdata")
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/perm1000/Threshold.perms.1000.qtl.RNA.rankZ.tpm.Rdata")
+load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/GM_snps.Rdata")
+
 #	Get all eQTL file paths
 All_eqtl_files_dir <- list.files("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/qtl/RNA/Complete_eQTL_Rdata", full.names = T)
 All_eqtl_files <- sub("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/qtl/RNA/Complete_eQTL_Rdata/","",All_eqtl_files_dir)
@@ -51,11 +50,22 @@ Ensembl_mouseID <- Ensembl_mouseID[ intersect(rownames(Ensembl_mouseID), All_eqt
 qtl_dir <- "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/qtl/RNA/Complete_eQTL_Rdata/"
 plot_dir <- "/hpcdata/ytakemon/Col4a5xDO/GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/RNA_qtl/Complete_eQTL_plots/"
 
+#	Check if snps exists
+GM_snps["JAX00474835",] #marker identified with highest lod score in region
+# looks up rsID  rs30867551 to make sure that it falls within Rfx3
+# No find another 
+load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/qtl/qtl.GFR.log.C2.192.Rdata")
+GFR_qtl <- qtl.GFR.log.C2.192$lod$A
+GFR_qtl <- GFR_qtl[GFR_qtl[,"chr"] == 19, ]
+GFR_qtl <- GFR_qtl[GFR_qtl[,"lod"] > 6, ]
+# Now try UNCHS047474
+GM_snps["UNCHS047474",] #Looks good!
+
 #Create empty array
-Marker <- "JAX00098823" #define marker of interest
-Fmn1_trans_eQTL <- array(0, c(length(All_eqtl_files), 2), dimnames = list (Ensembl_mouseID$ensembl_gene_id, c("MGI_ID", "LOD_score_at_JAX00098823")))
-Fmn1_trans_eQTL <- as.data.frame(Fmn1_trans_eQTL)
-Fmn1_trans_eQTL$MGI_ID <- Ensembl_mouseID$mgi_symbol
+Marker <- "UNCHS047474" #define marker of interest
+Rfx3_trans_eQTL <- array(0, c(length(All_eqtl_files), 2), dimnames = list (Ensembl_mouseID$ensembl_gene_id, c("MGI_ID", "LOD_score_at_UNCHS047474")))
+Rfx3_trans_eQTL <- as.data.frame(Rfx3_trans_eQTL)
+Rfx3_trans_eQTL$MGI_ID <- Ensembl_mouseID$mgi_symbol
 
 for (i in 1:length(All_eqtl_files)){
 	#	load saved qtl object and assing to new variable name, temp, for easier manipulation
@@ -63,23 +73,23 @@ for (i in 1:length(All_eqtl_files)){
 	load(All_eqtl_files_dir[i])
 	temp <- get(ls(pattern = "ENSMUSG")) 
 	#	temp$lod$A is a data frame with colnames: marker & lod	
-	Fmn1_trans_eQTL$LOD_score_at_JAX00098823[i] <- temp$lod$A[Marker, "lod"]
+	Rfx3_trans_eQTL$LOD_score_at_UNCHS047474[i] <- temp$lod$A[Marker, "lod"]
 	print(i)
-	print(Fmn1_trans_eQTL$LOD_score_at_JAX00098823[i])
+	print(Rfx3_trans_eQTL$LOD_score_at_UNCHS047474[i])
 }
 
-save(Fmn1_trans_eQTL, file = "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Fmn1_trans_eQTL.Rdata")
-#subset Fmn1_trans_eQTL by significance level  (something wrong with rownames of Fmn1)
+save(Rfx3_trans_eQTL, file = "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/eQTL/Rfx3_eQTL/Rfx3_trans_eQTL.Rdata")
+#subset Rfx3_trans_eQTL by significance level  (something wrong with rownames of Rfx3)
 
-Fmn1_trans_eQTL_Sig.0.05 <- Fmn1_trans_eQTL[Fmn1_trans_eQTL$LOD_score_at_JAX00098823 > 8.104061,]
+Rfx3_trans_eQTL_Sig.0.05 <- Rfx3_trans_eQTL[Rfx3_trans_eQTL$LOD_score_at_JAX00098823 > 8.104061,]
 
-Fmn1_trans_eQTL_Sig.0.1 <- Fmn1_trans_eQTL[Fmn1_trans_eQTL$LOD_score_at_JAX00098823 > 7.632182,]
+Rfx3_trans_eQTL_Sig.0.1 <- Rfx3_trans_eQTL[Rfx3_trans_eQTL$LOD_score_at_JAX00098823 > 7.632182,]
 
-Fmn1_trans_eQTL_Sig.0.63 <- Fmn1_trans_eQTL[Fmn1_trans_eQTL$LOD_score_at_JAX00098823 > 6.047843,]
+Rfx3_trans_eQTL_Sig.0.63 <- Rfx3_trans_eQTL[Rfx3_trans_eQTL$LOD_score_at_JAX00098823 > 6.047843,]
 
-write.table(Fmn1_trans_eQTL, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Fmn1_trans_eQTL_all.txt", sep="\t")
-write.table(Fmn1_trans_eQTL_Sig.0.05, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Fmn1_trans_eQTL_Sig.0.05.txt", sep="\t")
-write.table(Fmn1_trans_eQTL_Sig.0.1, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Fmn1_trans_eQTL_Sig.0.1.txt", sep="\t")
-write.table(Fmn1_trans_eQTL_Sig.0.63, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Fmn1_trans_eQTL_Sig.0.63.txt", sep="\t")
+write.table(Rfx3_trans_eQTL, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Rfx3_trans_eQTL_all.txt", sep="\t")
+write.table(Rfx3_trans_eQTL_Sig.0.05, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Rfx3_trans_eQTL_Sig.0.05.txt", sep="\t")
+write.table(Rfx3_trans_eQTL_Sig.0.1, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Rfx3_trans_eQTL_Sig.0.1.txt", sep="\t")
+write.table(Rfx3_trans_eQTL_Sig.0.63, "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Rfx3_trans_eQTL_Sig.0.63.txt", sep="\t")
 
 
