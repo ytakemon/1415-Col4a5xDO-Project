c3be98a57e98e4fde279364acefff8b2aca30b3b
ytakemon <yuka.takemon@gmail.com>
2017-01-24 16:27:15 -0500

    updated

--- Additional_Rfx3_plots.R
+++ Additional_Rfx3_plots.R
@@ -19,6 +19,7 @@ load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/GM_snps.Rdata")
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/qtl/RNA/qtl.RNA.rankZ.tpm.Rfx3.Rdata")
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/RNA_seq_Rdata/RNA_seq_tpm.Rdata")
 load("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/RNA_seq_Rdata/RNA_seq_rankZ_tpm.Rdata")
+load(file = "./GBRS_reconstruction/reconstruct/best.compiled.genoprob/Gene_allele.Rdata")
 pheno <- read.delim("./Phenotype/1415_master_pheno.txt", sep = "\t", header = TRUE)
 
 #clearn up pheno data
@@ -76,7 +77,6 @@ ggplot <- ggplot(ggdata, aes( x = Founder, y = Value, fill = Sex)) +
 pdf("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/Rfx3_TPM_at_Rfx3_UNC30148273_rs6279232_by_Founders.pdf", width = 10.0, height = 7.5)	
 print(ggplot)
 dev.off()
-
 
 ## Allele effect plot of GFR at Rfx3 locus#####################################################################
 fit <- lm(pheno$C2_log ~ pheno$Sex + best.genoprobs.192[,, "UNC30148273"], na.action = na.exclude)
@@ -240,6 +240,132 @@ ggplot <- ggplot(pheno, aes(y = Rfx3, x = Dnahc9)) +
 pdf("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/RankZ_Rfx3vDnahc9.pdf", width = 10.0, height = 7.5)
 print(ggplot)
 dev.off()
+
+## Correlation between Rfx3 exp vs GFR, male and female separeate##############################################################
+Gene_allele <- as.data.frame(Gene_allele)
+pheno$Rfx3_allele <- Gene_allele$ENSMUSG00000040929
+
+pheno_F <- pheno[pheno$Sex == "F",]
+pheno_F <- pheno_F[complete.cases(pheno_F$C2_log),]
+pheno_M <- pheno[pheno$Sex == "M",]
+pheno_M <- pheno_M[complete.cases(pheno_M$C2_log),]
+
+fit <- lm(C2_log ~ Rfx3_tpm, pheno_F)
+fitsum <- summary(fit)
+intcp <- signif(coef(fit)[1], 3)
+slope <- signif(coef(fit)[2], 3)
+pval <- signif(fitsum$coefficients[2,4], 3)
+r2 <- signif(fitsum$adj.r.squared, 3)
+eq <- paste("y = ", slope,"x ","+ ", intcp, ", ", "R^2 =", r2, ", ", " pval = ", pval, sep = "")
+ggplot <- ggplot(pheno_F, aes(y = C2_log, x = Rfx3_tpm)) +
+	geom_point(size = 2, aes(colour = Rfx3_allele)) +
+	geom_smooth(method = lm) +
+	annotate( "text" , y = 6.25, x = 3.5, label = eq, fontface = "bold", size = 5) + 
+	scale_x_continuous( "Rfx3 TPM", breaks = seq(0, 4.5, by = 0.5)) +
+	scale_y_continuous( "log-transformed C2 GFR", breaks = seq(0, 6.5, by = 0.5)) +
+	labs( title = "Rfx3 TPM vs Log-transformed C2 GFR (Females)") +
+	theme(plot.title = element_text(hjust = 0.5))  
+
+pdf("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/Rfx3vC2GFR_Females.pdf", width = 10.0, height = 7.5)
+print(ggplot)
+dev.off()
+
+fit <- lm(C2_log ~ Rfx3_tpm, pheno_M)
+fitsum <- summary(fit)
+intcp <- signif(coef(fit)[1], 3)
+slope <- signif(coef(fit)[2], 3)
+pval <- signif(fitsum$coefficients[2,4], 3)
+r2 <- signif(fitsum$adj.r.squared, 3)
+eq <- paste("y = ", slope,"x ","+ ", intcp, ", ", "R^2 =", r2, ", ", " pval = ", pval, sep = "")
+ggplot <- ggplot(pheno_M, aes(y = C2_log, x = Rfx3_tpm)) +
+	geom_point(size = 2, aes(colour = Rfx3_allele)) +
+	geom_smooth(method = lm) +
+	annotate( "text" , y = 6, x = 3.5, label = eq, fontface = "bold", size = 5) + 
+	scale_x_continuous( "Rfx3 TPM", breaks = seq(0, 6.5, by = 0.5)) +
+	scale_y_continuous( "log-transformed C2 GFR", breaks = seq(0, 7.5, by = 0.5)) +
+	labs( title = "Rfx3 TPM vs Log-transformed C2 GFR (Males)") +
+	theme(plot.title = element_text(hjust = 0.5))  
+
+pdf("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/Rfx3vC2GFR_Males.pdf", width = 10.0, height = 7.5)
+print(ggplot)
+dev.off()
+
+## Rfx3 TPM of founder alleles at Rfx3 locus vs GFR of founder alleles at Rfx3 locus (so basically combining the two plots from this morning
+
+fit <- lm(pheno$Rfx3_tpm ~ pheno$Sex + best.genoprobs.192[,, "UNC30148273"], na.action = na.exclude)
+#Female averages by strain at Snp marker
+cfit <- coef(fit)
+cfit[10] = 0 #set standard (ie strain compared to) to 0
+cfit <- cfit + cfit[1]  #add female 
+#ecfit <- exp(cfit) #exp fixes values back to natural numbers
+cfit_F <- as.data.frame(cfit)
+#Male averages by strain at snp marker
+cfit <- coef(fit)
+cfit[10] = 0 #set standard (ie strain compared to) to 0
+cfit <- cfit + cfit[1] + cfit[2] #add female and male = male (female = female, male = male + female)
+#ecfit <- exp(cfit) #exp fixes balues back to natural numbers
+cfit_M <- as.data.frame(cfit)
+data_coef <- cfit_F
+data_coef$M <- cfit_M$cfit
+colnames(data_coef) <- c("F","M")
+names(data_coef) <- c("F","M")
+data_coef <- data_coef[c(3:10),]
+rownames(data_coef) <- c("A","B","C","D","E","F","G","H")
+data_coef$founder <- rownames(data_coef)
+data_Rfx3 <- data_coef
+
+fit <- lm(pheno$C2_log ~ pheno$Sex + best.genoprobs.192[,, "UNC30148273"], na.action = na.exclude)
+#Female averages by strain at Snp marker
+cfit <- coef(fit)
+cfit[10] = 0 #set standard (ie strain compared to) to 0
+cfit <- cfit + cfit[1]  #add female 
+#ecfit <- exp(cfit) #exp fixes values back to natural numbers
+cfit_F <- as.data.frame(cfit)
+#Male averages by strain at snp marker
+cfit <- coef(fit)
+cfit[10] = 0 #set standard (ie strain compared to) to 0
+cfit <- cfit + cfit[1] + cfit[2] #add female and male = male (female = female, male = male + female)
+#ecfit <- exp(cfit) #exp fixes balues back to natural numbers
+cfit_M <- as.data.frame(cfit)
+data_coef <- cfit_F
+data_coef$M <- cfit_M$cfit
+colnames(data_coef) <- c("F","M")
+names(data_coef) <- c("F","M")
+data_coef <- data_coef[c(3:10),]
+rownames(data_coef) <- c("A","B","C","D","E","F","G","H")
+data_coef$founder <- rownames(data_coef)
+data_GFR <- data_coef
+
+names(data_Rfx3) <- c("Rfx3_F", "Rfx3_M", "Founder")
+names(data_GFR) <- c("GFR_F", "GFR_M", "Founder")
+
+data <- data_Rfx3
+data$GFR_F <- data_GFR$GFR_F
+data$GFR_M <- data_GFR$GFR_M
+
+##cor by sex
+ggplot <- ggplot(data, aes(x = Rfx3_F, y = GFR_F, colour = Founder)) + 
+	geom_point(size = 2) +
+	scale_x_continuous( "Rfx3 TPM") +
+	scale_y_continuous( "log-transformed C2 GFR") +
+	labs( title = "Rfx3 TPM vs Log-transformed C2 GFR by Founder(Females)") +
+	theme(plot.title = element_text(hjust = 0.5)) 
+
+pdf("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/Rfx3vGFR_Females_by_founder.pdf", width = 10.0, height = 7.5)
+print(ggplot)
+dev.off()
+
+ggplot <- ggplot(data, aes(x = Rfx3_M, y = GFR_M, colour = Founder)) + 
+	geom_point(size = 2) +
+	scale_x_continuous( "Rfx3 TPM") +
+	scale_y_continuous( "log-transformed C2 GFR") +
+	labs( title = "Rfx3 TPM vs Log-transformed C2 GFR by Founder(Male)") +
+	theme(plot.title = element_text(hjust = 0.5)) 
+
+pdf("./GBRS_reconstruction/reconstruct/best.compiled.genoprob/plot/Rfx3vGFR_Males_by_founder.pdf", width = 10.0, height = 7.5)
+print(ggplot)
+dev.off()
+
 
 
 
