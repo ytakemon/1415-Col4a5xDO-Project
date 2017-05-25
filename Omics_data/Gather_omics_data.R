2b09653b2e6c18ff3f5a68f330c4e5bcc64959dc
ytakemon <yuka.takemon@gmail.com>
2017-05-25 14:14:27 -0400

    Upload data

---
+++ Omics_data/Gather_omics_data.R
@@ -0,0 +1,8 @@
+# Gather public kidney datasets to check if we have genes that overlap.
+# The data sets collected are the following:
+##  HKUPP and UPDB refered in the Papadopoulos et al. paper.
+##  Proteomics dataset from Col4a3 KO mice in Muckova paper.
+##  Rinchen proteomics datasets: Isolated rat podocytes from induced kidney damage, human
+##  podocyte cell line with induced damage.
+
+# Extensive records should be kept to make sure we interpret data correctly.
