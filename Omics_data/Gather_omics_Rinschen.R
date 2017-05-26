cf53e52aa942fc6ce5e913acdd4294d3442c7802
ytakemon <yuka.takemon@gmail.com>
2017-05-26 09:03:04 -0400

    update

---
+++ Omics_data/Gather_omics_Rinschen.R
@@ -0,0 +1,30 @@
+# Gather public kidney datasets to check if we have genes that overlap.
+# The data sets collected are the following:
+##  HKUPP and UPDB refered in the Papadopoulos et al. paper.
+##  Proteomics dataset from Col4a3 KO mice in Muckova paper.
+##  Rinchen proteomics datasets: Isolated rat podocytes from induced kidney damage, human
+##  podocyte cell line with induced damage.
+
+# Extensive records should be kept to make sure we interpret data correctly.
+
+# YAP-mediated mechanotransduction determines the podocyte’s response to damage
+# Markus M. Rinschen,1,2,3,4 Florian Grahammer,5,6* Ann-Kathrin Hoppe,1,2*
+# Priyanka Kohli,1,2,3 Henning Hagmann,1,2 Oliver Kretz,5,6,7 Sabine Bertsch,1,2
+# Martin Höhne,1,2,3,4 Heike Göbel,8 Malte P. Bartram,1,2 Rajesh Kumar Gandhirajan,1
+# Marcus Krüger,2,3 Paul-Thomas Brinkkoetter,1,2 Tobias B. Huber,5,6,9,10
+# Martin Kann,1,2 Sara A. Wickström,3,11
+# Thomas Benzing,1,2,3,4† Bernhard Schermer1,2,3,4†
+
+# Data information provided by author:
+# This data demonstrates the ttest differences of logarithmized (log2) expression
+# values of every identified protein and the respective pvalue.
+# This matrix is based on the proteingroups.txt output of MaxQuant software.
+# Each column represents a different animal.
+# For details and more information, please check the raw data deposited at PRIDE
+# and the methods section of the manuscript.
+
+# Data retreived from supplement:
+# aaf8165_Data file S1.xlsx <- Proteomic analysis of PAN effect in rat glomeruli in Excel format.
+# aaf8165_Data file S2.txt <- Proteomic analysis of PAN effect in rat glomeruli in text format.
+# aaf8165_Data file S3.xlsx <- Proteomic analysis of PAN effect in human podocytes in Excel format.
+# aaf8165_Data file S4.txt <- Proteomic analysis of PAN effect in human podocytes in text format.
