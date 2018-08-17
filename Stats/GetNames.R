6ea2e34d6a239c8e1a280fe7e3e7f193eddecd69
ytakemon <yuka.takemon@gmail.com>
2018-08-17 08:21:28 -0400

    Gather all sample names

---
+++ Stats/GetNames.R
@@ -0,0 +1,6 @@
+library(stringr)
+setwd("/projects/ytakemon/Col4a5xDO/civet_run/")
+list <- list.files(pattern = "fastq.gz")
+list <- str_sub(list,15,)
+
+write.table(list, "./Name_list.txt", row.names = FALSE, quote = FALSE, sep = "\t", col.names = FALSE)
