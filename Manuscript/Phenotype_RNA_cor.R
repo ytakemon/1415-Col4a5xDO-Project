b5da341701e5e60c4ae5bcdba5b7a971f2f22901
Yuka Takemon <yuka.takemon@gmail.com>
2020-02-22 16:12:21 -0800

    redo correlation analysis

---
+++ Manuscript/Phenotype_RNA_cor.R
@@ -0,0 +1,42 @@
+# Run this script
+# R-3.6.0 --no-save < Phenotype_RNA_cor.R > Rout/Phenotype_RNA_cor.Rout 2>&1 &
+
+# Create a heatmap of correlation between gene expression and ACR @ 15wks and GFR at 14wks
+
+library(pacman)
+p_load(tidyverse, broom)
+
+cor_dir <- "/projects/marralab/ytakemon_prj/Col4a5/"
+load(paste0(cor_dir,"Data/RNAseq/RNA_seq_rankZ_tpm.Rdata"))
+load(paste0(cor_dir,"Data/Col4a5xDO_192data_YT.Rdata"))
+
+RNA_seqZ <- RNA_seqZ %>% as_tibble(., rownames = "MouseID")
+select_pheno <- Pheno %>% mutate(ACR15WK_log = log(ACR15WK)) %>%
+  select(MouseID, ACR15WK_log, C2_log)
+
+identical(RNA_seqZ$MouseID, select_pheno$MouseID) # good to compare directly
+
+# Create function to get results
+getRNACor <- function(gene, pheno, res){
+
+  rna <- RNA_seqZ %>% select(MouseID, all_of(gene))
+  df <- full_join(select_pheno, rna, by = "MouseID")
+
+  fit <- cor.test(df[,pheno], df[,gene], use = "complete.obs", method = "pearson" ) %>%tidy()
+
+  output <- fit %>% pull(res)
+  return(output)
+}
+
+cor <- tibble(EnsID = colnames(RNA_seqZ)[-1],
+              acr_cor = NA,
+              acr_pval  = NA,
+              gfr = NA,
+              gfr_pval = NA) %>%
+        mutate(
+          acr_cor = map_dbl(EnsID, getRNACor, "ACR15WK_log", "estimate"),
+          acr_pval = map_dbl(EnsID, getRNACor, "ACR15WK_log", "p.value"),
+          gfr = map_dbl(EnsID, getRNACor, "C2_log", "estimate"),
+          gfr_pval = map_dbl(EnsID, getRNACor, "C2_log", "p.value"))
+
+write_csv(cor, paste0(cor_dir,"Data/RNAseq/RNAseq_phenotype_cor_15wk.csv"))
